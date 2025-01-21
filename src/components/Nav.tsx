5594c2100159bca02e5bd9dfcf47028c41fb9c48
Benjamin AUBRON <benjamin.aubron@outlook.com>
2025-01-21 17:55:44 +0100

    first commit

---
+++ src/components/Nav.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div className='max-w-screen-xl mx-auto flex items-center justify-between text-white bg-black border-b-[1px] border-[#1E2029] py-6'>
+      <div className='text-2xl font-semibold'>
+        <span className='text-[#8F9094]'>&#123; </span>
+        <span>Benjamin</span>
+        <span className='text-[#8F9094]'> &#125;</span>
+      </div>
+      <div className='flex items-center gap-4'>
+        <div>Home</div>
+        <div>Projects</div>
+      </div>
+    </div>
+  )
+}
