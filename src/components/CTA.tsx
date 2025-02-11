58b671dc426f2c12c9ce634e2d3ad858fbcc2e46
Benjamin AUBRON <benjamin.aubron@outlook.com>
2025-02-11 15:35:35 +0100

    CTA responsive - done

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -5,20 +5,20 @@ import { PiReadCvLogoFill } from "react-icons/pi";
 
 export default function CTA() {
   return (
-    <div id="home" className='relative max-w-screen-xl mx-auto pt-20 px-4 h-screen'>
+    <div id="home" className='relative max-w-screen-xl mx-auto pt-20 px-6 h-screen'>
       <div>
-        <div className='text-[--primaryColorDark] text-lg md:text-4xl font-semibold mt-20'>Hey there 👋 ! I'm</div>
-        <h1 className='text-neutral-100 font-extrabold text-5xl md:text-8xl py-4 lg:py-10'>Benjamin AUBRON</h1>
-        <h2 className='text-neutral-500 text-base md:text-5xl font-semibold max-w-screen-lg py-4 lg:py-6'><span className='text-neutral-100'>Next.js Front-end Engineer.</span> Huge interest in building things. I'm looking for new challenges.</h2>
-        <div className='text-neutral-400 text-xs md:text-3xl font-medium py-12'>👩🏻‍💻 Currently specializing in Frontend (React / Next)</div>
-        <div className="flex flex-wrap items-center justify-start gap-3 md:gap-6 text-neutral-100 pt-4">
-          <a href="" className='flex items-center gap-2 bg-neutral-900 hover:bg-neutral-800 rounded-lg py-2 px-2.5 md:px-4 text-xs md:text-xl font-semibold'>
+        <div className='text-[--primaryColorDark] text-2xl md:text-4xl font-semibold pt-20'>Hey there 👋 ! I'm</div>
+        <h1 className='text-neutral-100 font-extrabold text-6xl md:text-8xl pt-0 py-4 lg:py-10'>Benjamin AUBRON</h1>
+        <h2 className='text-neutral-500 text-2xl md:text-5xl font-semibold max-w-screen-lg py-4 lg:py-6'><span className='text-neutral-100'>Next.js Front-end Engineer.</span> Huge interest in building things. I'm looking for new challenges.</h2>
+        <div className='text-neutral-400 text-lg md:text-3xl font-medium py-12'>👩🏻‍💻 Currently specializing in Frontend (React / Next)</div>
+        <div className="flex flex-wrap items-center justify-start gap-3 md:gap-6 text-neutral-100 text-xl pt-4">
+          <a href="" className='flex items-center gap-2 bg-neutral-900 hover:bg-neutral-800 rounded-lg py-2 px-2.5 md:px-4 font-semibold'>
             <FaGithub className="text-base md:text-lg text-[--primaryColorDark] "/>
             GitHub</a>
-          <a href="" className='flex items-center gap-2 bg-neutral-900 hover:bg-neutral-800 rounded-lg py-2 px-2.5 md:px-4 text-xs md:text-xl font-semibold'>
+          <a href="" className='flex items-center gap-2 bg-neutral-900 hover:bg-neutral-800 rounded-lg py-2 px-2.5 md:px-4 font-semibold'>
             <PiReadCvLogoFill className="text-base md:text-xl text-[--primaryColorDark] "/>
             Resume</a>
-          <a href="" className='flex items-center gap-2 bg-neutral-900 hover:bg-neutral-800 rounded-lg py-2 px-2.5 md:px-4 text-xs md:text-xl font-semibold'>
+          <a href="" className='flex items-center gap-2 bg-neutral-900 hover:bg-neutral-800 rounded-lg py-2 px-2.5 md:px-4 font-semibold'>
             <MdEmail className="text-base md:text-xl text-[--primaryColorDark] "/>
              Email</a>
         </div>
