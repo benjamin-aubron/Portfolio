f764f1311952b7b3b783864b005aa0ab7c25f18d
Benjamin AUBRON <benjamin.aubron@outlook.com>
2025-02-12 15:47:26 +0100

    build started

---
+++ dist/static/chunks/src_app_page_tsx_23f34f._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_page_tsx_23f34f._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_page_tsx_23f34f._.js",
+  "chunks": [
+    "static/chunks/src_ccd901._.js",
+    "static/chunks/node_modules_51fcc7._.js"
+  ],
+  "source": "dynamic"
+});
