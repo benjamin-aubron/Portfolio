682f12655055c4e334cb506736891458064ac14c
Benjamin AUBRON <benjamin.aubron@outlook.com>
2025-02-12 19:23:40 +0100

    about description - done

--- dist/server/app/page.js
+++ dist/server/app/page.js
@@ -7,6 +7,7 @@ runtime.loadChunk("server/chunks/ssr/src_app_globals_b80590.css");
 runtime.loadChunk("server/chunks/ssr/node_modules_ce97a5._.js");
 runtime.loadChunk("server/chunks/ssr/node_modules_next_dist_client_components_forbidden-error_b4e556.js");
 runtime.loadChunk("server/chunks/ssr/node_modules_next_dist_client_components_unauthorized-error_d758e6.js");
+runtime.loadChunk("server/chunks/ssr/_ab7bc3._.js");
 runtime.loadChunk("server/chunks/ssr/node_modules_react-icons_lu_index_mjs_02a885._.js");
 runtime.loadChunk("server/chunks/ssr/node_modules_react-icons_fa_index_mjs_a1beb0._.js");
 runtime.loadChunk("server/chunks/ssr/node_modules_react-icons_pi_index_mjs_7022dd._.js");
@@ -15,6 +16,5 @@ runtime.loadChunk("server/chunks/ssr/node_modules_react-icons_io5_index_mjs_1653
 runtime.loadChunk("server/chunks/ssr/node_modules_react-icons_io_index_mjs_c24c6e._.js");
 runtime.loadChunk("server/chunks/ssr/node_modules_react-icons_lib_b806b5._.js");
 runtime.loadChunk("server/chunks/ssr/node_modules_next_dist_esm_build_templates_app-page_5dfd58.js");
-runtime.loadChunk("server/chunks/ssr/_ab7bc3._.js");
 runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/page/actions.js [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
 module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-page.js?page=/page { MODULE_0 => \"[project]/src/app/layout.tsx [app-rsc] (ecmascript, Next.js server component)\", MODULE_1 => \"[project]/node_modules/next/dist/client/components/not-found-error.js [app-rsc] (ecmascript, Next.js server component)\", MODULE_2 => \"[project]/node_modules/next/dist/client/components/forbidden-error.js [app-rsc] (ecmascript, Next.js server component)\", MODULE_3 => \"[project]/node_modules/next/dist/client/components/unauthorized-error.js [app-rsc] (ecmascript, Next.js server component)\", MODULE_4 => \"[project]/src/app/page.tsx [app-rsc] (ecmascript, Next.js server component)\" } [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
