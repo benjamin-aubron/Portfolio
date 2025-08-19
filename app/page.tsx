3cdeeacc87af875e87771c027c1fcf5cbeb27542
Benjamin AUBRON <benjamin.aubron@outlook.com>
2025-08-19 10:35:02 +0200

    clean base

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center text-4xl justify-center">
+      Hello
+    </div>
+  );
+}
