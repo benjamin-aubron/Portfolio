25a715bdfd619f4ec04adf0dcb54710676b4a1cc
Benjamin AUBRON <benjamin.aubron@outlook.com>
2025-09-06 17:35:14 +0200

    languageSwitch fixed

--- components/LanguageSwitch.tsx
+++ components/LanguageSwitch.tsx
@@ -1,23 +1,31 @@
 "use client";
-
+import { useRouter } from "next/navigation";
+import { useLocale } from "next-intl";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
 } from "@/components/ui/select";
 
 export default function LanguageSwitch() {
-  return (
-    <Select>
-      <SelectTrigger className="flex justify-center items-center md:w-[40px] w-[160px] bg-neutral-800 md:bg-inherit cursor-pointer">
-        <SelectValue placeholder="🇫🇷" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectItem value="fr">🇫🇷</SelectItem>
-        <SelectItem value="en">🇬🇧</SelectItem>
-      </SelectContent>
-    </Select>
-  );
+	const router = useRouter();
+	const locale = useLocale();
+
+	const handleLanguageChange = (newLocale: string) => {
+		router.push(`/${newLocale}`);
+	};
+
+	return (
+		<Select value={locale} onValueChange={handleLanguageChange}>
+			<SelectTrigger className="flex justify-center items-center md:w-[40px] w-[160px] bg-neutral-800 md:bg-inherit cursor-pointer">
+				<SelectValue placeholder={locale === "fr" ? "🇫🇷" : "🇬🇧"} />
+			</SelectTrigger>
+			<SelectContent>
+				<SelectItem value="fr">🇫🇷</SelectItem>
+				<SelectItem value="en">🇬🇧</SelectItem>
+			</SelectContent>
+		</Select>
+	);
 }
